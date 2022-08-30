e5d4ff5d49994a08566016344c70e2b92a938954
unknown <krishayg>
2022-08-29 22:04:21 -0700

    a

--- game/routes.py
+++ game/routes.py
@@ -7,7 +7,7 @@ from flask_login import login_user,logout_user, login_required, current_user
 @app.route("/")
 @app.route("/home")
 def home_page():
-    return render_template("home.html")
+    return render_template("question.html")
 @app.route("/join",methods=["GET","POST"])
 def join_page():
     form=JoinForm()
